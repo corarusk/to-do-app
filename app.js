0204f669fa3d2541feaa5c9859dce53971a9b547
corahowell <corarusk@outlook.com>
2019-09-13 14:32:57 -0500

    checkpoint 12 dom: completed

---
+++ app.js
@@ -0,0 +1,37 @@
+function onReady() {
+  
+  const ADD_TODO_FORM = document.getElementById('addToDoForm');
+  const NEW_TODO_TEXT = document.getElementById('newToDoText');
+  const TODO_LIST = document.getElementById('toDoList');
+
+
+  ADD_TODO_FORM.addEventListener('submit', (event) => {
+    event.preventDefault();
+    // get the text
+    let title = NEW_TODO_TEXT.value;
+
+    let newLi = document.createElement('li') // <li></li>
+    let checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click', function(event) {
+      //console.log(event);
+      //this.parentElement represents the button's <li> parent
+      TODO_LIST.removeChild(this.parentElement);
+    })
+
+    newLi.textContent = title;
+    newLi.appendChild(checkbox);
+    newLi.appendChild(deleteBtn);
+    TODO_LIST.appendChild(newLi);
+    NEW_TODO_TEXT.value = "";
+  });
+};
+
+window.onload = function() {
+  alert("The window has loaded!");
+  onReady();
+};
